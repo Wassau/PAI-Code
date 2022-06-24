6f0966503f9463278e8145a1ff2c0c6e3a4c8183
Daniel Delgado <dadelgadoa@unal.edu.co>
2022-06-24 13:45:02 -0500

    Update MPPyDC.ino

--- MPPyDC/MPPyDC.ino
+++ MPPyDC/MPPyDC.ino
@@ -4,9 +4,8 @@
   Written by BARRAGAN and modified by Scott Fitzgerald
   modified by Daniel Delgado 11/06/2022
 *********/
-
 #include <Stepper.h>
-#include "Arduino.h"
+
 /*
   // pin definitions
   #define pinLimitadorArco 1 // *** limits ***
@@ -32,41 +31,14 @@
   //#define CS  5
 
 */
-//////////////////////////////////////
-/////////// Definitions //////////////
-//////////////////////////////////////
-
-// esp32 constants
-#define analogInputMax 4095
-
-// pins stepper motor
+// Definitions
 #define IN1 26 // ULN2003 Motor Driver Pins
 #define IN2 25
 #define IN3 33
 #define IN4 32
-
-// pins joystick
-//#define pinJoystickX 4
-//#define pinJoystickY 15
-
-
-// pins potenciometer
-#define pinPot 35
-#define pinCalibration 21
-
-// pins  button
-//#define EMERGENCY_BUTTON_PIN 22
-#define pinGreenBtn 22
-#define BUZZER_PIN 27
-#define pinNorthRed 15
-#define pinWestYellow 16
-#define pinSouthBlue 4
-#define pinEastGreen 34
-
-// pins DC motor
-#define pinDCMotorDriveEn 2
-#define pinDCMotorDriveIN1 12
-#define pinDCMotorDriveIN2 14
+#define pinDcMotorPWM 12 // define pin of data of the servo
+#define pinJoystickX 4
+#define pinJoystickY 15
 
 // Math constants
 #define PI 3.1415926535897932384626433832795
@@ -86,7 +58,6 @@ int max_duty_cycle = (int)(pow(2, resolution) - 1);
 float wait = (1.0 / max_duty_cycle) * 1000.0;
 
 const int car_radius_gear = 11;// mm
-const int velMotorDC_max = 120;// rpm
 const int radius = 800;// mm
 const int N_rack = 40 * 2; // 25 * 32 number of racks by the number of theeth in each rack
 const double NperArcLength = N_rack / (PI*radius);
@@ -102,17 +73,10 @@ double u_carNext = 0;
 int arcAngleActual = 0;
 int arcAngleNext = 0;
 
-
 // Location to move
 const double latitude = 4.637;
 const double longitude = -74.083;
 
-// joystick
-bool isManual = true;
-int joystickXvalue = 0;
-int joystickYvalue = 0;
-int mapX = 0;
-int mapY = 0;
 
 // potenciometer
 volatile double potValue = 0;
@@ -123,82 +87,104 @@ double potValueAngle_offset = 0;
 volatile int potDegreeAngle = 0;
 float EMA_a = 0.6;      //initialization of EMA alpha
 int EMA_S = 0;          //initialization of EMA S
+const int sizeArray = 10;
 const int sizeArray = 2;
 int EMA_S_array[sizeArray];
 int counter = 0;
 volatile int potRealAngle = 0;
 
-// stpm car variable and constants
-int numOfTeethMoved = 0;
-const double teethPermm = 40 / 1000;
-const int diameterCarGear = 24; // mm
-
-// EMERGENCY
-bool IS_EMERGENCY = false;
-//int EMERGENCY_BUTTON_PIN_STATE = HIGH; // negative logic
-bool isCalibrated = false;
-bool calibrationLedState = LOW;
-int greenBtnState = HIGH; // negative logic
-
-volatile int pinNorthRedState = HIGH;
-volatile int pinWestYellowState = HIGH;
-volatile int pinSouthBlueState = HIGH;
-volatile int pinEastGreenState = HIGH;
+bool isManual = true;
+int joystickXvalue = 0;
+int joystickYvalue = 0;
+int mapX = 0;
+int mapY = 0;
 
 
 Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4); // initialize the stepper library
 
-
-//*******************************************
-//***************** MAIN ********************
-//*******************************************
-
-
 void setup() {
-  Serial.begin(115200); // initialize the serial port
-  pinMode(pinCalibration, OUTPUT);
-  pinMode(pinDCMotorDriveIN1, OUTPUT);
-  pinMode(pinDCMotorDriveIN2, OUTPUT);
-  pinMode(pinDCMotorDriveEn, OUTPUT);
-  pinMode(pinGreenBtn, INPUT_PULLUP);
-  pinMode(pinNorthRed, INPUT_PULLUP);
-  pinMode(pinWestYellow, INPUT_PULLUP);
-  pinMode(pinSouthBlue, INPUT_PULLUP);
-  pinMode(pinEastGreen, INPUT_PULLUP);
-
-  //attachInterrupt(EMERGENCY_BUTTON_PIN, ISR_EMERGENCY, FALLING);
-  attachInterrupt(pinNorthRed, ISR_btnNorthRed, FALLING);
-  attachInterrupt(pinWestYellow, ISR_btnWestYellow, FALLING);
-  attachInterrupt(pinSouthBlue, ISR_btnSouthBlue, FALLING);
-  attachInterrupt(pinEastGreen, ISR_btnEastGreen, FALLING);
   ledcSetup(dcMotorChannel, freq, resolution);
-  ledcAttachPin(pinDCMotorDriveEn, dcMotorChannel);
+  ledcAttachPin(pinDcMotorPWM, dcMotorChannel);
+
   myStepper.setSpeed(5); // set the speed at 5 rpm
+  Serial.begin(115200); // initialize the serial port
+  Serial.println("Motor code");
+
   setIsManual(true);
+
+
 }
 
 void loop() {
-  if (!IS_EMERGENCY) {
-    while (!isCalibrated) {
-      isCalibratedUpdate();
-    }
-    potToAngleUpdate();
-    if (counter == -1) {
-      if (getIsManual()) {
-        Serial.println(F("is manual"));
-        manualMov();
-      } else {
-        automaticMov(42.05, 297.38);
-      }
-    }
 
+  if (getIsManual()) {
+    manualMov();
+  } else {
+    automaticMov(42.05, 297.38);
   }
-  counter++;
 }
 
-//*******************************************
-//*************** FUNCTIONS *****************
-//*******************************************
+
+// Type of movement
+void manualMov() {
+  int joystick_offset = 150;
+  // offset jaystick -> mapX: 78 mapY: 95
+  joystickXvalue = analogRead(pinJoystickX);
+  joystickYvalue = analogRead(pinJoystickY);
+  mapX = map(joystickXvalue, 0, 4095, -255, 255);
+  Serial.print(F("mapX: "));
+  Serial.print(mapX);
+  if ((mapX > joystick_offset) || (mapX < -joystick_offset)) {
+    pwmMov(mapX);
+  }
+  mapY = map(joystickYvalue, 0, 4095, -255, 255);
+  Serial.print(F(" mapY: "));
+  Serial.println(mapY);
+  if (mapY > joystick_offset) {
+    motorArcMov(1);
+  }
+  if (mapY < -joystick_offset) {
+    motorArcMov(-1);
+  }
+}
+
+void automaticMov(float elNextInput, float azNextInput) {
+  setNextPos(elNextInput, azNextInput);
+  motorCarMov();
+  delay(1000);
+
+  motorArcMov(numStepsArc());
+  delay(1000);
+
+  showPos();
+  delay(1000);
+}
+
+// motors movement
+void motorCarMov() {
+  if ((u_carNext < 600) && (u_carNext > -600)) {
+    // function of car movement
+    updateValues();
+  }
+
+}
+void motorArcMov(int numSteps) {
+  arcAngleNextCalc();
+  myStepper.step(numSteps);
+  updateValues();
+  if ((arcAngleNext < 80) && (arcAngleNext > -80)) {
+    //
+  }
+}
+
+// arc and stepper functions
+void arcAngleNextCalc() {
+  arcAngleNext = (int)atan2(radius * sin(elNext), radius * cos(elNext) * cos(azNext));
+}
+
+int numStepsArc() {
+  return unitaryStepPerDegree * arcAngleNext;
+}
 
 void potToAngleUpdate() {
   //btnsUpdateState();
@@ -219,143 +205,7 @@ void potToAngleUpdate() {
   }
 }
 
-void isCalibratedUpdate() {
-  (digitalRead(pinGreenBtn) == LOW) ? isCalibrated = true : isCalibrated = isCalibrated;
-  pot_offset = analogRead(pinPot);
-  Serial.print(F("pot_offset NEW: "));
-  Serial.println(pot_offset);
-  if (pot_offset > 1050 && pot_offset < 1080) {
-    Serial.print(F("calibrationLedState NEW: "));
-    Serial.println(calibrationLedState);
-    calibrationLedState = HIGH;
-    digitalWrite(pinCalibration, calibrationLedState);
-  } else {
-    calibrationLedState = LOW;
-    digitalWrite(pinCalibration, calibrationLedState);
-  }
-}
-
-int average() {
-  int aux = 0;
-  for (int i = 0; i < sizeArray; i++) {
-    aux = + aux + EMA_S_array[i];
-  }
-  return aux / sizeArray;
-}
-/////////////////////////////////////////////
-//////////// Type of movement ///////////////
-/////////////////////////////////////////////
-
-void btnsUpdateState() {
-
-  pinNorthRedState = digitalRead(pinNorthRedState);
-  delay(15);
-  pinWestYellowState = digitalRead(pinWestYellowState);
-  delay(15);
-  pinSouthBlueState = digitalRead(pinSouthBlueState);
-  delay(15);
-  pinEastGreenState = digitalRead(pinEastGreenState);
-  delay(15);
-
-  Serial.print(F("pinNorthRedState: "));
-  Serial.println(pinNorthRedState);
-  Serial.print(F("pinWestYellowState: "));
-  Serial.println(pinWestYellowState);
-  Serial.print(F("pinSouthBlueState: "));
-  Serial.println(pinSouthBlueState);
-  Serial.print(F("pinEastGreenState: "));
-  Serial.println(pinEastGreenState);
-}
-void manualMov() {
-  
-}
-
-void xorIF(){
-  if( (pinNorthRedState == LOW && pinSouthBlueState == HIGH) || (pinNorthRedState == HIGH && pinSouthBlueState == LOW) ){
-    (pinNorthRedState == LOW) ? motorArcMov(1) : motorArcMov(-1);
-  }
-
-  if( (pinWestYellowState == LOW && pinEastGreenState == HIGH) || (pinWestYellowState == HIGH && pinEastGreenState == LOW) ){
-    (pinWestYellowState == LOW) ? pwmMov(255) : pwmMov(-255);
-  }
-}
-
-//void joystickFun() {
-//  int joystick_offset = 150;
-//  // offset jaystick -> mapX: 78 mapY: 95
-//  joystickXvalue = analogRead(pinJoystickX);
-//  joystickYvalue = analogRead(pinJoystickY);
-//  mapX = map(joystickXvalue, 0, analogInputMax, (-analogInputMax - 800 / 2 ), (analogInputMax - 800 / 2 ));
-//  Serial.print(F("mapX: "));
-//  Serial.print(mapX);
-//  if ((mapX > joystick_offset) || (mapX < -joystick_offset)) {
-//    if (numOfTeethMoved < 30 && numOfTeethMoved > -30) {
-//      unsigned long time_o = millis() ;
-//
-//      unsigned long time_f = millis() ;
-//      //numOfTeethMovedUpdate(time_o, time_f, (255 / 4) * mapX / (analogInputMax / 2));
-//    }
-//  }
-//  mapY = map(joystickYvalue, 0, analogInputMax, -255, 255);
-//  Serial.print(F(" mapY: "));
-//  Serial.println(mapY);
-//  if (mapY > joystick_offset && potRealAngle <= 80) { //&& encoderValue <= 4 North to south is positive
-//    motorArcMov(1);
-//  }
-//  if (mapY < -joystick_offset && potRealAngle >= -80) { // && encoderValue >= -4
-//    motorArcMov(-1);
-//  }
-//}
-
-void automaticMov(float elNextInput, float azNextInput) {
-  setNextPos(elNextInput, azNextInput);
-  motorCarMov();
-  delay(1000);
-
-  motorArcMov(numStepsArc());
-  delay(1000);
-
-  showPos();
-  delay(1000);
-
-  Serial.print(F("Acomoda el arco en su posición de incio y oprime el botón"));
-
-}
-/////////////////////////////////////////////
-//////// motors movement functions //////////
-/////////////////////////////////////////////
-void motorCarMov() {
-  if ((u_carNext < 600) && (u_carNext > -600)) {
-    // function of car movement
-    updateValues();
-  }
-
-}
-void motorArcMov(int numSteps) {
-  arcAngleNextCalc();
-  myStepper.step(numSteps);
-  updateValues();
-  if ((arcAngleNext < 80) && (arcAngleNext > -80)) {
-    //
-  }
-}
-
-/////////////////////////////////////////////
-/////// arc and stepper functions ///////////
-/////////////////////////////////////////////
-void arcAngleNextCalc() {
-  arcAngleNext = (int)atan2(radius * sin(elNext), radius * cos(elNext) * cos(azNext));
-}
-
-int numStepsArc() {
-  return unitaryStepPerDegree * arcAngleNext;
-}
-
-
-
-/////////////////////////////////////////////
-/////// car and dc motor functions //////////
-/////////////////////////////////////////////
+// car and dc motor functions
 int numPulsesCar() {
   u_carNext = radius * cos(elNext) * sin(azNext); // milimeters
   //d_car = sqrt(pow(radius, 2) * cos(elNext) - pow(radius, 2) * pow(cos(elNext), 2) * pow(cos(azNext), 2)) - u_carActual;
@@ -377,63 +227,10 @@ int pulsesPerN() {
 }
 
 void pwmMov(int dutyCycle) {
-  //  digitalWrite(pinDCMotorDriveIN1, HIGH);
-  //  digitalWrite(pinDCMotorDriveIN2, HIGH);
-  (dutyCycle > 0) ? setMovDCMotorForward() : setMovDCMotorBackwards();
-
-  for(int i = 0; i <= 50; i++){
-    ledcWrite(dcMotorChannel, abs(i));
-  }
-//  for(int i = 100; i <= 200; i++){
-//    ledcWrite(dcMotorChannel, abs(i));
-//  }
-//  for(int i = 200; i >= 100; i--){
-//    ledcWrite(dcMotorChannel, abs(i));
-//  }
-  for(int i = 50; i >= 0; i--){
-    ledcWrite(dcMotorChannel, abs(i));
-  }
-  //delay(15);
+  ledcWrite(dcMotorChannel, dutyCycle);
+  delay(15);
 }
 
-void numOfTeethMovedUpdate(unsigned long time_o, unsigned long time_f, int dutyCycle) {
-  unsigned long deltat = time_f - time_o;//14
-  Serial.print(F("delta de tiempo: "));
-  Serial.println(deltat);
-  double omega = (dutyCycle) / 255 * velMotorDC_max;
-  Serial.print(F("omega: "));
-  Serial.println(omega);
-  double vel = PI * diameterCarGear * omega / 60; // mm/s
-  Serial.print(F("velocidad: "));
-  Serial.println(vel);
-  numOfTeethMoved = + (int)teethPermm * vel * deltat;
-  Serial.print(F("numOfTeethMoved: "));
-  Serial.println(numOfTeethMoved);
-}
-
-
-void stopDCMotor() {
-  Serial.println(F(" DC Motor stopped "));
-  digitalWrite(pinDCMotorDriveIN1, LOW);
-  digitalWrite(pinDCMotorDriveIN2, LOW);
-}
-
-void setMovDCMotorForward() {
-  Serial.println(F(" DC motor Moving Forward "));
-  digitalWrite(pinDCMotorDriveIN1, HIGH);
-  digitalWrite(pinDCMotorDriveIN2, LOW);
-}
-
-void setMovDCMotorBackwards() {
-  Serial.println(F(" DC motor Moving Backwards "));
-  digitalWrite(pinDCMotorDriveIN1, LOW);
-  digitalWrite(pinDCMotorDriveIN2, HIGH);
-}
-
-
-/////////////////////////////////////////////
-//////////// auxiliar functions /////////////
-/////////////////////////////////////////////
 void setNextPos(float elNextInput, float azNextInput) {
   elNext = elNextInput;// 42.05
   azNext = azNextInput; // 297.38
@@ -459,36 +256,30 @@ void updateValues() {
   u_carActual = u_carNext;
 }
 
-//////////////////
-// interruptions /
-//////////////////
-void ISR_btnNorthRed() {
-  motorArcMov(1);
-}
-void ISR_btnWestYellow() {
-  pwmMov(255);
-  delay(100);
-}
-void ISR_btnSouthBlue() {
-  motorArcMov(-1);
-}
-void ISR_btnEastGreen() {
-  pwmMov(-255);
-  delay(100);
-}
 
-void ISR_EMERGENCY() {
-  //(digitalRead(EMERGENCY_BUTTON_PIN_STATE) == LOW) ? IS_EMERGENCY = true : IS_EMERGENCY = IS_EMERGENCY;
-  IS_EMERGENCY = true;
-  stopDCMotor();
-  myStepper.setSpeed(0);
-  Serial.print(F("**********************"));
-  Serial.print(F("IS_EMERGENCY: "));
-  Serial.println(IS_EMERGENCY);
-  Serial.print(F("**********************"));
-  delay(10000);
-}
 
+void isCalibratedUpdate() {
+  (digitalRead(pinGreenBtn) == LOW) ? isCalibrated = true : isCalibrated = isCalibrated;
+  pot_offset = analogRead(pinPot);
+  Serial.print(F("pot_offset NEW: "));
+  Serial.println(pot_offset);
+  if (pot_offset > 1050 && pot_offset < 1080) {
+    Serial.print(F("calibrationLedState NEW: "));
+    Serial.println(calibrationLedState);
+    calibrationLedState = HIGH;
+    digitalWrite(pinCalibration, calibrationLedState);
+  } else {
+    calibrationLedState = LOW;
+    digitalWrite(pinCalibration, calibrationLedState);
+  }
+}
+int average() {
+  int aux = 0;
+  for (int i = 0; i < sizeArray; i++) {
+    aux = + aux + EMA_S_array[i];
+  }
+  return aux / sizeArray;
+}
 
 
 // show values in serial monitor
