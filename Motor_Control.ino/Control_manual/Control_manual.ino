5a0b5e0258941d50042aeb92b9dee51fcb8c8e26
Wassau <dachaconr@unal.edu.co>
2022-06-27 20:11:01 -0500

    MotorDC_funcionando

--- Motor_Control.ino/Control_manual/Control_manual.ino
+++ Motor_Control.ino/Control_manual/Control_manual.ino
@@ -2,7 +2,7 @@
 
 // pins stepper motor
 #define DIR_STPM 16 //  Motor Driver Pins
-#define PULSE 4 
+#define PULSE 4
 #define EN_STPM 17
 #define pinPot 32
 
@@ -11,9 +11,9 @@ int Angletostep = 0;
 // pins  button
 #define EMERGENCY_BUTTON_PIN 2
 #define pinNorth 35
-#define pinWest 36
+#define Carromas 36
 #define pinSouth 39
-#define pinEast 34
+#define Carromenos 34
 
 
 // pins DC motor
@@ -31,6 +31,19 @@ const int resolution = 8;
 int max_duty_cycle = (int)(pow(2, resolution) - 1);
 float wait = (1.0 / max_duty_cycle) * 1000.0;
 
+// setting PWM_STPM properties
+const int freq2 = 5000;
+const int MotorChannel2 = 1;
+const int resolution2 = 14;
+int max_duty_cycle2 = (int)(pow(2, resolution) - 1);
+float wait2 = (1.0 / max_duty_cycle) * 1000.0;
+
+
+
+bool moveR = false;
+bool moveL =  false;
+bool moveforwards = false;
+bool moveback = false;
 // potenciometer
 
 volatile double potValue = 0;
@@ -45,7 +58,7 @@ const int sizeArray = 2;
 int EMA_S_array[sizeArray];
 int counter = 0;
 volatile int potRealAngle = 0;
-
+int Duty = 0;
 bool IS_EMERGENCY = false; // EMERGENCY
 int posCarro = 0; // encoder
 volatile int pinNorthRedState = HIGH;
@@ -55,74 +68,134 @@ volatile int pinEastGreenState = HIGH;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200); 
-  pinMode(ENCA,INPUT);
-  pinMode(ENCB,INPUT);  
-    pinMode(pinDCMotorDriveIN1, OUTPUT);
+  Serial.begin(115200);
+  pinMode(ENCA, INPUT);
+  pinMode(ENCB, INPUT);
+  pinMode(DIR_STPM, OUTPUT);
+  pinMode(EN_STPM, OUTPUT);
+  pinMode(PULSE, OUTPUT);
+  pinMode(pinDCMotorDriveIN1, OUTPUT);
   pinMode(pinDCMotorDriveIN2, OUTPUT);
   pinMode(pinNorth, INPUT_PULLUP);
-  pinMode(pinWest, INPUT_PULLUP);
+  pinMode(Carromas, INPUT_PULLUP);
   pinMode(pinSouth, INPUT_PULLUP);
-  pinMode(pinEast, INPUT_PULLUP);
-  attachInterrupt(pinNorth, ISR_btnNorthRed, FALLING);
-  attachInterrupt(pinWest, ISR_btnWestYellow, FALLING);
-  attachInterrupt(pinSouth, ISR_btnSouthBlue, FALLING);
-  attachInterrupt(pinEast, ISR_btnEastGreen, RISING);
-  attachInterrupt(digitalPinToInterrupt(ENCA),readEncoder,RISING);
+  pinMode(Carromenos, INPUT_PULLUP);
+  attachInterrupt(EMERGENCY_BUTTON_PIN, ISR_EMERGENCY, FALLING);
+  attachInterrupt(pinNorth, ISR_btnNorthRed, RISING);
+  attachInterrupt(Carromas, ISR_btnWestYellow, RISING);
+  attachInterrupt(pinSouth, ISR_btnSouthBlue, RISING);
+  attachInterrupt(Carromenos, ISR_btnEastGreen, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENCA), readEncoder, RISING);
   ledcSetup(dcMotorChannel, freq, resolution);
   ledcAttachPin(pinDCMotorDriveEn, dcMotorChannel);
+    ledcSetup(MotorChannel2, freq2, resolution2);
+  ledcAttachPin(PULSE, MotorChannel2);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-//    if (!IS_EMERGENCY) {
-//    if (counter == -1) {
-//      if (getIsManual()) {
-//        Serial.println(F("is manual"));
-//        manualMov();
-//      } else {
-//       // automaticMov(42.05, 297.38);
-//      }
-//    }
-//
-//  }
-//  counter++;
-//}
-manualMov();
+  //    if (!IS_EMERGENCY) {
+  //    if (counter == -1) {
+  //      if (getIsManual()) {
+  //        Serial.println(F("is manual"));
+  //        manualMov();
+  //      } else {
+  //       // automaticMov(42.05, 297.38);
+  //      }
+  //    }
+  //
+  //  }
+  //  counter++;
+  //}
+
+
+  if (moveR) {
+    digitalWrite(DIR_STPM, HIGH);
+    digitalWrite (EN_STPM, HIGH);
+    for (int x = 0; x < 100 ; x++) { // define CCW or CW rotation
+      digitalWrite(PULSE, HIGH);
+      digitalWrite (EN_STPM, LOW);
+      delayMicroseconds(2);
+      digitalWrite(PULSE, LOW);
+      digitalWrite (EN_STPM, HIGH);
+      delayMicroseconds(2);
+    }
+    delay(100);
+     moveR = false;
+  }
+  if (moveL) {
+        digitalWrite(DIR_STPM, LOW);
+    digitalWrite (EN_STPM, HIGH);
+    for (int x = 0; x < 100 ; x++) { // define CCW or CW rotation
+      digitalWrite(PULSE, HIGH);
+      digitalWrite (EN_STPM, HIGH);
+      delayMicroseconds(2);
+      digitalWrite(PULSE, LOW);
+      digitalWrite (EN_STPM, LOW);
+      delayMicroseconds(2);
+    }
+    delay(100);
+    moveL = false;
+  }
+  if(moveforwards){
+  digitalWrite(pinDCMotorDriveIN1, LOW ); //Switch between this HIGH and LOW to change direction
+  digitalWrite(pinDCMotorDriveIN2,  HIGH);
+  ledcWrite(dcMotorChannel, 180);
+  delay(100);
+    digitalWrite(pinDCMotorDriveIN1, LOW );
+  digitalWrite(pinDCMotorDriveIN2,  LOW);
+  moveforwards =false;
+  }
+   if(moveback){
+    digitalWrite(pinDCMotorDriveIN1, HIGH); //Switch between this HIGH and LOW to change direction
+  digitalWrite(pinDCMotorDriveIN2, LOW);
+  ledcWrite(dcMotorChannel, 180);
+  delay(100);
+  digitalWrite(pinDCMotorDriveIN1, LOW); //STOP
+  digitalWrite(pinDCMotorDriveIN2, LOW);
+   moveback =false;
+}
 }
 //////////////////
 // Motor Movement /
 //////////////////
 
 
-void setStepmotor(int dirPin, int Duty, int target){
-  if (dirPin == 1){
+void setStepmotor(int dirPin, int Duty, int target) {
+  if (dirPin == 1) {
     // Set motor direction clockwise
-    Angletostep = (STEPS_PER_REV / 360)*target;
-     digitalWrite (EN_STPM,HIGH);
-      for(int x = 0; x < Angletostep ; x++) {  // define CCW or CW rotation
-        digitalWrite(PULSE,LOW);
-        delayMicroseconds(Duty);
-        digitalWrite(PULSE,HIGH);
-        delayMicroseconds(Duty);
-      }
+    Angletostep = (STEPS_PER_REV / 360) * target;
+    digitalWrite (EN_STPM, HIGH);
+    digitalWrite (DIR_STPM, HIGH);
+    for (int x = 0; x < Angletostep ; x++) { // define CCW or CW rotation
+      digitalWrite(PULSE, LOW);
+      digitalWrite (EN_STPM, HIGH);
+      delayMicroseconds(Duty);
+      digitalWrite(PULSE, HIGH);
+      digitalWrite (EN_STPM, LOW);
+      delayMicroseconds(Duty);
+    }
   }
-      else {
-   // Set motor direction counterclockwise
-      for(int x = 0; x < Angletostep ; x++) { // define CCW or CW rotation
-        digitalWrite(PULSE,HIGH);
-        delayMicroseconds(Duty);
-        digitalWrite(PULSE,LOW);
-        delayMicroseconds(Duty);
-      }
+  else {
+    // Set motor direction counterclockwise
+    digitalWrite (DIR_STPM, LOW);
+    for (int x = 0; x < Angletostep ; x++) { // define CCW or CW rotation
+      digitalWrite(PULSE, HIGH);
+      digitalWrite (EN_STPM, HIGH);
+      delayMicroseconds(Duty);
+      digitalWrite(PULSE, LOW);
+      digitalWrite (EN_STPM, LOW);
+      delayMicroseconds(Duty);
+
+    }
   }
- }
- void stopDCMotor() {
+}
+void stopDCMotor() {
   Serial.println(F(" DC Motor stopped "));
   digitalWrite(pinDCMotorDriveIN1, LOW);
   digitalWrite(pinDCMotorDriveIN2, LOW);
 }
- void btnsUpdateState() {
+void btnsUpdateState() {
 
   pinNorthRedState = digitalRead(pinNorthRedState);
   delay(15);
@@ -143,23 +216,23 @@ void setStepmotor(int dirPin, int Duty, int target){
   Serial.println(pinEastGreenState);
 }
 void manualMov() {
-  
+
 }
 //////////////////
 // Sensors /
 //////////////////
-void readEncoder(){
+void readEncoder() {
   int b = digitalRead(ENCB); // Calcula la posición en Grados del motor (verificar PPR)
-  if(b > 0){
+  if (b > 0) {
     posCarro++;
   }
-  else{
+  else {
     posCarro--;
   }
 }
 void potToAngleUpdate() {
   //btnsUpdateState();
-    Serial.print(F("pinNorthRedState: "));
+  Serial.print(F("pinNorthRedState: "));
   Serial.println(pinNorthRedState);
   Serial.print(F("pinWestYellowState: "));
   Serial.println(pinWestYellowState);
@@ -194,22 +267,19 @@ int average() {
 //////////////////
 
 void ISR_btnNorthRed() {
-  setStepmotor(1 , 5 , 5);  // (direction (1 or -1) , velocity (microsec/step) -> Rising value goes slowly , angle)
+     moveR= true;
 }
 void  ISR_btnWestYellow() {
-  digitalWrite(pinDCMotorDriveIN1, LOW ); //Switch between this HIGH and LOW to change direction
-  digitalWrite(pinDCMotorDriveIN2,  HIGH);
-  ledcWrite(dcMotorChannel,50);
-  Serial.println("Interrupcion Verde");
+      moveforwards=true;
 }
-void ISR_btnSouthBlue() {
-  setStepmotor(-1 , 5 , 5); // (direction (1 or -1) , velocity (step/microsec) , angle)
-}
+
 void  ISR_btnEastGreen() {
-  digitalWrite(pinDCMotorDriveIN1, HIGH); //Switch between this HIGH and LOW to change direction
-  digitalWrite(pinDCMotorDriveIN2, LOW);
-  ledcWrite(dcMotorChannel,50);
-  Serial.println("Interrupcion azul");
+      moveback=true;
+}
+
+void ISR_btnSouthBlue() {
+  //setStepmotor(-1 , 5 , 5); // (direction (1 or -1) , velocity (step/microsec) , angle)
+   moveL=true;
 }
 
 void ISR_EMERGENCY() {
